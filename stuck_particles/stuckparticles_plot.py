44823ef93e8311572426dd0ace9737b8045a8a0c
Raymond Edwards <raymondedwards95@gmail.com>
2018-05-16 21:32:32 +0200

    add vector-field to plotVelocity and plotTrajectories

--- stuck_particles/stuckparticles_plot.py
+++ stuck_particles/stuckparticles_plot.py
@@ -19,7 +19,7 @@ import math
 
 from netCDF4 import Dataset
 
-from parcels import Field, Fieldset
+from parcels import Field, FieldSet
 
 try:
     import matplotlib.pyplot as plt
@@ -27,7 +27,7 @@ except:
     print "ERROR: matplotlib not found"
 
 
-def plotVelocity(vector, field="U", coords=None, show=None, savefile=None, vmax=None):
+def plotVelocity(vector, field="vector", coords=None, show=None, savefile=None, vmax=None):
     """ plot results of stp.getVelocity() """
     if savefile is None:
         show = True
@@ -53,6 +53,10 @@ def plotVelocity(vector, field="U", coords=None, show=None, savefile=None, vmax=
         vel = U
     elif field == "V":
         vel = V
+    elif field == "vector":
+        pass
+    elif field == "absolute":
+        vel = np.sqrt(np.power(U, 2) + np.power(V, 2))
     else:
         print "plotVelocity(): parameter 'field' is incorrect. Expected 'U' or 'V'"
 
@@ -68,10 +72,16 @@ def plotVelocity(vector, field="U", coords=None, show=None, savefile=None, vmax=
         time = "start"
 
     plt.figure()
-    plt.contourf(lons, lats, vel, vmin=vmin, vmax=vmax)
+
+    if field == "U" or field == "V" or field == "absolute":
+        plt.contourf(lons, lats, vel, vmin=vmin, vmax=vmax)
+    elif field == "vector":
+        color = np.hypot(U, V)
+        plt.quiver(lon, lat, U, V, color)
+
     plt.plot(plon, plat, 'o', markersize=5, color="red")
     plt.plot(mesh_x.flatten(), mesh_y.flatten(), 'o', markersize=3, color="black")
-    plt.title("field {} at t={}".format(field, time))
+    # plt.title("field {} at t={}".format(field, time))
     plt.xlabel("longitude")
     plt.ylabel("latitude")
     plt.colorbar()
@@ -350,9 +360,15 @@ def plotCoasts(coasts, show=None, savefile=None):
 #     z = np.ma.filled(file.variables["z"], np.nan)
 
 
-def plotTrajectories(filename, ocean_particles=True, coast_particles=True, coasts=None, savefile=None):
-    """ Plot trajectories of particles in a trajectories-file (*.nc) """
+def plotTrajectories(filename, ocean_particles=True, coast_particles=True, field=None, coasts=None, show=None, savefile=None):
+    """ Plot trajectories of particles in a trajectories-file (*.nc)
     ### to do: filters
+    """
+    if savefile is None:
+        show = True
+    if show is None and savefile is not None:
+        show = False
+
     file = Dataset(filename, "r")
 
     lon = np.ma.filled(file.variables["lon"], np.nan)
@@ -387,17 +403,29 @@ def plotTrajectories(filename, ocean_particles=True, coast_particles=True, coast
                 for field in coasts:
                     plt.contourf(field.grid.lon, field.grid.lat, field.data[0].astype(np.bool), alpha=0.5, cmap="Greys")
 
-            elif isinstance(coasts, FieldSet)
-                # coasts is a FieldSet
-                try:
-                    coasts.F_all
-                except NameError:
-                    print "plotTrajectories(): cannot find coastfields (F_all) in parameter 'coast'"
-
+        elif isinstance(coasts, FieldSet):
+            # coasts is a FieldSet
+            try:
+                coasts.F_all
                 plt.contourf(coasts.F_all.grid.lon, coasts.F_all.grid.lat, coasts.F_all.data[0].astype(np.bool), alpha=0.5, cmap="Greys")
+            except NameError:
+                print "plotTrajectories(): cannot find coastfields (F_all) in parameter 'coast'"
 
         else:
             print "plotTrajectories(): 'coasts' is not a list of coasts or a field"
+
+    if field is not None:
+        if field.U.grid == field.V.grid:
+            lon_ = field.U.grid.lon
+            lat_ = field.U.grid.lat
+        else:
+            print "plotTrajectories(): grids of field.U and field.V are not the same, trying to continue"
+            lon_ = field.U.grid.lon
+            lat_ = field.U.grid.lat
+
+        ### index [0] is probably wrong !!!
+        color = np.hypot(field.U.data[0], field.V.data[0])
+        plt.quiver(lon_, lat_, field.U.data[0], field.V.data[0], color)
 
     if savefile is not None:
         plt.savefig(savefile)
